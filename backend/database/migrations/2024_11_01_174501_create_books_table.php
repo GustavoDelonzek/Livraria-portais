9b6f12c47194fb5e628978798d90b4dc51f8d013
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2024-11-01 11:11:49 -0300

    Inicio projeto

---
+++ backend/database/migrations/2024_11_01_174501_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('publisher_id');
+            $table->year('published_year');
+            $table->string('genre');
+            $table->decimal('price', 8, 2);
+            $table->integer('stock');
+            $table->text('description');
+            $table->timestamps();
+        
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+            $table->foreign('publisher_id')->references('id')->on('publishers')->onDelete('cascade');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ backend/database/migrations/2024_11_01_184710_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // ID do usuário
+            $table->unsignedBigInteger('book_id'); // ID do livro
+            $table->unsignedTinyInteger('rating')->default(0); // Avaliação
+            $table->text('comment')->nullable(); // Comentário
+            $table->timestamps();
+        
+            // Chaves estrangeiras
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
