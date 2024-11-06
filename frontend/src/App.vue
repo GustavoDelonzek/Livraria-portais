14e335bc75f6bd57a6584d26d93d86c03c02f62b
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2024-11-05 23:38:29 -0300

    Prototipo sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,67 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import { 
+    initAccordions, 
+    initCarousels, 
+    initCollapses, 
+    initDials, 
+    initDismisses, 
+    initDrawers, 
+    initDropdowns, 
+    initModals, 
+    initPopovers, 
+    initTabs, 
+    initTooltips } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initAccordions();
+    initCarousels();
+    initCollapses();
+    initDials();
+    initDismisses();
+    initDrawers();
+    initDropdowns();
+    initModals();
+    initPopovers();
+    initTabs();
+    initTooltips();
+})
+
+
 </script>
 
 
-<style scoped>
 
-</style>
+<style scoped></style>
 
 <script>
 import Header from './components/Header.vue'
-
+import SideBar from './components/SideBar.vue'
 export default {
   name: 'App',
   components: {
-    Header
+    Header, 
+    SideBar
   }
 }
 </script>
 
 <template>
-  <Header></Header>
+ 
+  <div class="flex h-screen">
+    
+    <SideBar class="w-64 bg-gray-800 text-white fixed h-full"/>
+    
+    <div class="flex-1 flex flex-col ml-64">
+      
+    
+      <main class="p-4 pt-20">
+        <RouterView />
+      </main>
+      
+    </div>
+  </div>
+  
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <script setup>
+import SideBar from '@/components/SideBar.vue';
+
 </script>
 
 <template>
   <main>
+    
     <h1>This is Home page</h1>
   </main>
 </template>
+
+
+<script>
+export default {
+  name: "home",
+  components: {
+    SideBar
+  }
+}
+</script>
